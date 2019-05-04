08744c3992a13f53133630bb48b324628c652315
matteopasina <matteopasina@gmail.com>
2019-05-04 21:01:59 +0200

    Multi currency conversion unittest

---
+++ test/test_multi_converter.py
@@ -0,0 +1,52 @@
+import unittest
+from data.constants import *
+from core.multi_converter import MultiCurrencyConverter
+from core.rates_retriever import RateRetriever
+
+
+class TestMultiCurrencyConverter(unittest.TestCase):
+    def setUp(self):
+        retriever = RateRetriever(MOCKED_RATES_FILE)
+        self.converter = MultiCurrencyConverter(retriever)
+
+    def test_multi_convert(self):
+        post_request = {
+            "to_currency": "EUR",
+            "amounts": [
+                {
+                    "from_currency": "USD",
+                    "amounts": ["1", "2"]
+                },
+                {
+                    "from_currency": "PLN",
+                    "amounts": ["37", "2,89", "9.3"]
+                }
+            ]
+        }
+        expected_response = {
+            "to_currency": "EUR",
+            "converted-amounts": [
+                {
+                    "from_currency": "USD",
+                    "converted_amounts": [
+                        "0.8923",
+                        "1.7846"
+                    ]
+                },
+                {
+                    "from_currency": "PLN",
+                    "converted_amounts": [
+                        "8.6469",
+                        "0.675393",
+                        "2.17341"
+                    ]
+                }
+            ]
+        }
+
+        response = self.converter.multi_convert(post_request)
+        self.assertDictEqual(response, expected_response)
+
+
+if __name__ == '__main__':
+    unittest.main()
